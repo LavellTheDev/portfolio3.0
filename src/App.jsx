92de985ab3de24e1e068d6a107642e73bcd67cd2
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2021-12-26 23:27:08 -0500

    finished hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
+import { useState } from "react";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-        <Topbar/>
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
