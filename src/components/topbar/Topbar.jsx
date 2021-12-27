3f2835d914c41c23d52775b6a96a7ca9e8574f4a
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2021-12-26 23:27:19 -0500

    hamburger\

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import {Person , Mail} from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen , setMenuOpen }) {
     return (
-        <div className="topbar active">
+        <div className={ "topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">lavellwesley.</a>
@@ -18,7 +18,7 @@ export default function Topbar() {
                 </div>
                 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={() =>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
