0fd310bdaa0628d80b9fa2472b48a9ab611280a0
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2021-12-28 01:34:59 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
         <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
