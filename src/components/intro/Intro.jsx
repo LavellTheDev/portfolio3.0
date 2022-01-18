261172222c6441b6fb7c2eb4f4d15c1692633e06
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2022-01-18 12:47:39 -0500

    changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/kzakat_c-01.png" alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
