ff1a5ad95f4e1570eddac330dbf2a6b45966edd8
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2022-01-29 22:05:13 -0500

    starting portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className = "portfolio" id="portfolio">
-
+            <h1>Portfolio</h1>
         </div>
     )
 }
