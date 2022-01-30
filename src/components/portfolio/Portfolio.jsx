2fd5b63d50ab8aee7a725c7efcc32b7ba59df319
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2022-01-29 22:24:14 -0500

    more changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,19 @@ export default function Portfolio() {
     return (
         <div className = "portfolio" id="portfolio">
             <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3></h3>
+                </div>
+            </div>
         </div>
     )
 }
